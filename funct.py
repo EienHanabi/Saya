b73706441e1d0ec20682eff9dc7d0f235f5fd00c
Pierrick Garcia <pierrick.garcia@hotmail.fr>
2021-02-10 11:10:32 +0100

    Add an argument to rec to choose between 1 and 20 songs

--- funct.py
+++ funct.py
@@ -1,4 +1,5 @@
 import time
+import math
 import discord
 import aiosqlite
 from Arcapi import AsyncApi
@@ -181,6 +182,12 @@ async def ptt_recommendation(message):
         await message.channel.send("> Erreur: Aucun code Arcaea n'est lié a ce compte Discord (*!register*)")
         return
 
+    nb_scores = 5
+    if len(message.content.split(" ")) > 1:
+        if message.content.split(" ")[1].isdigit():
+            if 1 <= int(message.content.split(" ")[1]) <= 20:
+                nb_scores = int(message.content.split(" ")[1])
+
     api_ = AsyncApi(user_code=code)
     data = await api_.scores()
     scores = []
@@ -208,14 +215,15 @@ async def ptt_recommendation(message):
     scores_others = filter(lambda scores: scores['score'] < 10000000, scores_others)
     scores_others_2 = filter(lambda scores: scores['score'] < 10000000, scores_others_2)
 
-    # 10 recommendations : Oldest scores from top 30
-    ptt_rec += sorted(scores_top_30, key=itemgetter('time_played'), reverse=False)[0:10]
-    # 5 recommendations : Oldest scores not in top 30 with Chart Constant > PTT - 1
+    half_nb_scores = math.floor(nb_scores / 2)
+    # Max 1/4 recommendations : Oldest scores not in top 30 with Chart Constant > PTT - 1
     filtered_scores = filter(lambda scores: scores['constant'] > PTT - 1, scores_others)
-    ptt_rec += sorted(filtered_scores, key=itemgetter('time_played'), reverse=False)[0:5]
-    # 5 recommendations : Oldest scores not in top 30 with PTT - 1 >= Chart Constant > PTT - 2
+    ptt_rec += sorted(filtered_scores, key=itemgetter('time_played'), reverse=False)[0:int(math.ceil(half_nb_scores/2))]
+    # Max 1/4 recommendations : Oldest scores not in top 30 with PTT - 1 >= Chart Constant > PTT - 2
     filtered_scores_2 = filter(lambda scores: PTT - 1 >= scores['constant'] > last_top_30['rating'] - 2, scores_others_2)
-    ptt_rec += sorted(filtered_scores_2, key=itemgetter('time_played'), reverse=False)[0:5]
+    ptt_rec += sorted(filtered_scores_2, key=itemgetter('time_played'), reverse=False)[0:int(math.floor(half_nb_scores/2))]
+    # Rest of recommendations : Oldest scores from top 30
+    ptt_rec += sorted(scores_top_30, key=itemgetter('time_played'), reverse=False)[0:nb_scores - len(ptt_rec)]
     # Sort by time_played
     ptt_rec = sorted(ptt_rec, key=itemgetter('time_played'), reverse=False)
 
@@ -238,7 +246,7 @@ async def get_help(message):
                                "> !best [1-30]: Sends the Top [1-30] plays (Default: 30)\n"
                                "> !help: Sends this message\n"
                                "> !profile: Displays the user profile\n"
-                               "> !rec: Sends 15 recommended plays to increase PTT\n"
+                               "> !rec [1-20]: Sends [1-20] recommended plays to increase PTT (Default: 5)\n"
                                "> !recent: Sends the latest play\n"
                                "> !register: Links a user code to a Discord account")
 
